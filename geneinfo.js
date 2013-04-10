e4e860fa42b67a411a81f58de14b047ac12885d2
gkarthik_92 <xraydiffraction@gmail.com>
2013-04-10 16:05:01 +0530

    Converted code to a library mygeneinfo.js with three basic functions.

---
+++ geneinfo.js
@@ -0,0 +1,45 @@
+var mygeneinfo = (function(){
+	return {
+		getJSON: function(query_text,limit,callback){
+			$.getJSON("http://mygene.info/query?q="+query_text+"&limit="+limit+"&callback=gene_padding", function(data){
+				callback(data);
+		});
+		},
+		printJSON: function(query_text,limit,element_id){
+			$("#"+element_id).html("Loading...");
+			$.getJSON("http://mygene.info/query?q="+query_text+"&limit=50&callback=gene_padding", function(data){
+				$("#"+element_id).html(syntaxHighlight(data));
+			});
+		},
+		printPrettyJSON: function(query_text,limit,element_id){
+			$("#"+element_id).html("Loading...");
+			$.getJSON("http://mygene.info/query?q="+query_text+"&limit=50&callback=gene_padding", function(data){
+				var json = JSON.stringify(data, null, 2);
+				$("#"+element_id).html("<pre>"+json+"</pre>");
+			});
+		}
+	};
+}());
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    console.log(json);
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
--- script.js
+++ script.js
@@ -6,44 +6,30 @@ $("#input-main").keyup(function(){
 	query_text=$("#input-main").val();
 	if(query_text!="")
 	{
-		$("#loading-text").html("Loading...");
-		$.getJSON("http://mygene.info/query?q="+query_text+"&limit=50&callback=gene_padding", function(data){
-			table = prettyPrint(data,{maxDepth:10});
-			raw_json=syntaxHighlight(data);
-			$("#loading-text").html("");
-			$("#content").html(table);	
-		});	
+		mygeneinfo.printPrettyJSON(query_text,20,"content");
 	}
 });
 
 $("#raw-json").click(function(){
-$("#content").html(raw_json);
+	query_text=$("#input-main").val();
+	if(query_text!="")
+	{
+		mygeneinfo.printJSON(query_text,20,"content");
+	}
+});
+
+$("#pretty-extlib-json").click(function(){
+	mygeneinfo.getJSON(query_text,20,function(data){
+		$("#content").html(prettyPrint(data));
+	});
 });
 
 $("#pretty-json").click(function(){
-$("#content").html(table);
+	query_text=$("#input-main").val();
+	if(query_text!="")
+	{
+		mygeneinfo.printPrettyJSON(query_text,20,"content");
+	}
 });
 
 });
-
-function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-         json = JSON.stringify(json, undefined, 2);
-    }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
